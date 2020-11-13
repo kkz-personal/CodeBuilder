8135aa3681509fca07bb53f74f79a8e3d691c337
kmz <maozhi.ke@okcoin.net>
2020-11-13 15:16:37 +0800

    --

---
+++ src/main/java/codebuilder/Main.java
@@ -0,0 +1,12 @@
+package codebuilder;
+
+/**
+ * @author maozhi.k
+ * @date 2020/11/13 2:53 PM
+ */
+public class Main {
+    public static void main(String[] args){
+        VelocityFactory velocityFactory = new VelocityFactory();
+        velocityFactory.generateTable();
+    }
+}
