5e03dbf5589672f73daf5500ba83ce835728d62c
kmz <maozhi.ke@okcoin.net>
2020-12-21 11:38:36 +0800

    --

--- src/main/java/codebuilder/Main.java
+++ src/main/java/codebuilder/Main.java
@@ -1,12 +1,23 @@
 package codebuilder;
 
+import codebuilder.mysql.MysqlFactory;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 /**
  * @author maozhi.k
  * @date 2020/11/11 11:57 AM
  */
 public class Main {
-    public static void main(String[] args){
-        VelocityFactory factory = new VelocityFactory();
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String path = "";
+        if (args.length != 0){
+            path = args[0];
+        }
+        path = URLDecoder.decode(path,"utf-8");
+        VelocityFactory factory = new VelocityFactory(path);
+        MysqlFactory.init(path);
         factory.generateTable();
     }
 }
